4cd57f520bc85645cfbe5c8f7b162037e0b8fd51
ssbajracharya <ssbajracharya@outlook.com>
2020-04-08 13:45:35 -0500

    fiexed checkLegal Co-authored-by: Bek Zhuma <beka.uk@gmail.com> Co-authored-by: k.w.maguire.ii@gmail.com <k.w.maguire.ii@gmail.com>

--- src/Rummikub.js
+++ src/Rummikub.js
@@ -33,6 +33,118 @@ const initializeGame = (ctx) => {
   return G
 }
 
+// This functions returns array with sub-arrays of all the combinations in the Game board.
+function getAllCombinations(board) {
+  const rows = []
+  for (let i = 0; i < board.length; i += 16) {
+    rows.push(board.slice(i, i + 16))
+  }
+  const allCombinations = []
+  let tempArray = []
+
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < rows[i].length; j++) {
+      if (rows[i][j] !== null) {
+        tempArray.push(rows[i][j])
+      }
+      if (rows[i][j] === null) {
+        if (tempArray.length > 0) {
+          allCombinations.push(tempArray)
+          tempArray = []
+        }
+      }
+    }
+    if (tempArray.length > 0) {
+      allCombinations.push(tempArray)
+    }
+  }
+  return allCombinations
+}
+
+/////////////////////////////////////////////////////////////////////
+// The main function to check if the move in the GameBoard is Legal
+const checkLegal = (board) => {
+  let resultArray = []
+  const combinations = getAllCombinations(board)
+  for (let i = 0; i < combinations.length; i++) {
+    if (combinations[i].length < 3)
+      return false
+  }
+
+  // Function returns true if combination are in valid Group
+  function checkRuns(combinations) {
+    let resultArray = []
+    const allEqual = (arr) => arr.every(item => item === arr[0])
+    const isConsecutive = (arr) =>{
+      for(let i=0; i<arr.length - 1; i++){
+        if(arr[i + 1]-arr[i] !== 1) return false
+      }
+      return true
+    }
+    for (let i = 0; i < combinations.length; i++) {
+      let numArray = []
+      let alphaArray = []
+      for (let j = 0; j < combinations[i].length; j++) {
+        let tile = combinations[i][j]
+        if(numArray.length > 0 && tile === 'JK'){
+          numArray.push((parseInt(numArray[numArray.length - 1]) + 1).toString())
+        }
+        else if(numArray.length === 0 && tile === 'JK'){
+          if(combinations[i][j + 1] == "JK"){
+          numArray.push((parseInt((combinations[i][j + 2]).replace(/[^0-9]/g, "")) - 2).toString())
+          }
+          else{numArray.push((parseInt((combinations[i][j + 1]).replace(/[^0-9]/g, "")) - 1).toString())}
+        }
+        else{
+          let alpha = tile.replace(/[0-9]/g, "")
+          let num = tile.replace(/[^0-9]/g, "")
+          alphaArray.push(alpha)
+          numArray.push(num)
+        }
+      }
+      if(!isConsecutive(numArray) || !allEqual(alphaArray)){
+        resultArray.push(false)
+      }
+      else{resultArray.push(true)}
+    }
+    return resultArray
+  }
+  
+  function checkGroups(combinations) {
+    let resultArray = []
+    const allEqual = (arr) => arr.every(item => item === arr[0])
+    for (let i = 0; i < combinations.length; i++) {
+      if(combinations[i].length > 4) return false
+      let numArray = []
+      let alphaArray = []
+      for (let j = 0; j < combinations[i].length; j++) {
+        let tile = combinations[i][j]
+        if(tile === 'JK'){continue}
+        else{
+          let alpha = tile.replace(/[0-9]/g, "")
+          let num = tile.replace(/[^0-9]/g, "")
+          alphaArray.push(alpha)
+          numArray.push(num)
+        }
+      }
+      if(!allEqual(numArray) || allEqual(alphaArray)){
+        resultArray.push(false)
+      }
+      else{resultArray.push(true)}
+        
+    }
+    return resultArray
+  }
+  let resultGroup = checkGroups(combinations)
+  let resultRuns = checkRuns(combinations)
+  for(let i = 0; i < resultRuns.length; i++){
+    if(resultGroup[i] === resultRuns[i]){
+      return false
+    }
+  }
+  return true
+}
+
 const MoveTile = (G, ctx, {fromLocation, fromX, fromY, toLocation, toX, toY}) => {
   const convertXYtoIndex = (location, x, y) => {
     switch (location) {
@@ -58,6 +170,13 @@ const MoveTile = (G, ctx, {fromLocation, fromX, fromY, toLocation, toX, toY}) =>
   origin[originIndex] = dTile
 }
 
+const FinishTurn = (G, ctx) => {
+  if (!checkLegal(G.cells)) {
+    return INVALID_MOVE
+  }
+  ctx.events.endTurn()
+}
+
 const PullTile = {
   move: (G, ctx, playerID) => {
     const playerRack = G.players[playerID]
@@ -70,17 +189,6 @@ const PullTile = {
     }
   },
   client: false
-}
-
-const checkLegal = (board) => {
-  return true
-}
-
-const FinishTurn = (G, ctx) => {
-  if (!checkLegal(G.cells)) {
-    return INVALID_MOVE
-  }
-  ctx.events.endTurn()
 }
 
 const Rummikub = {
