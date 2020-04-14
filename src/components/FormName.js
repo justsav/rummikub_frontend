55afe4873b3dc4d3f7a89d48778892067d2c76ae
beknazar1 <5906333+beknazar1@users.noreply.github.com>
2020-04-14 00:16:26 -0500

    fix form related console errors

--- src/components/FormName.js
+++ src/components/FormName.js
@@ -5,7 +5,7 @@ import '../lobby.css'
 export default function FormName({ playerName, setPlayerName }) {
   return (
     <Form onSubmit={(e) => e.preventDefault()}>
-      <Form.Group controlId="playerId" id='form-name'>
+      <Form.Group id="form-name">
         <Form.Label id='name-label'>First, tell us your name:</Form.Label>
           <Form.Control
             id='name-input'
